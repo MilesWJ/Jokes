5589f2573df8745e4c47b9317064ff7fe36d918e
Miles James <milesw.james@gmail.com>
2021-03-17 14:12:31 -0700

    Delete joke.py

--- joke.py
+++
@@ -1,6 +0,0 @@
-class Joke(object):
-    def __init__(self, setup, punchline, url, json_data):
-        self.setup = setup
-        self.punchline = punchline
-        self.url = url
-        self.json_data = json_data
